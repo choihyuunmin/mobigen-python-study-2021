2b5d11f666f508a6966e96f47e867edd9574f2b2
최현민 <75130734+choihyuunmin@users.noreply.github.com>
2021-07-11 00:54:23 +0900

    Create word_count_hyunmin.py

---
+++ week03/homework/word_count_hyunmin.py
@@ -0,0 +1,22 @@
+import os
+
+path = os.getcwd()
+
+print(path)
+
+with open(f'{path}/wiki_python.txt', encoding='utf-8') as f:
+    file = f.read()
+    words = file.split()
+    
+word_count = {}
+
+for word in words:
+    if word in word_count:
+        word_count[word] += 1
+    else:
+        word_count[word] = 1
+
+rank  = sorted(word_count.items(), key=lambda x : x[1], reverse=True)
+
+for i in range(10):
+    print(rank[i])
